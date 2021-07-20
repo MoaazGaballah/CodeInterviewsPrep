a389d9de7ba5bb171bdc870447fd1523d1fb9ab4
Moaaz GABALLAH <gaballah.moaaz@gmail.com>
2021-07-20 19:17:54 +0300

    Adding the problem description

--- src/AlgoExpret/SortStack/SortStack.java
+++ src/AlgoExpret/SortStack/SortStack.java
@@ -4,6 +4,26 @@ import java.util.ArrayList;
 
 public class SortStack {
 
+    /*
+     * Problem: Write a funtion that takes in an array of integers representing
+     * a stack, recursively sorts the stack in place (i.e., doesn't create
+     * a brand new array), and returns it.
+     * The array must be treated as a stack, with the end of the array as the
+     * top if the stack. Therefore, you're only allowed to
+     * - Pop elements from the top of the stack by removing elements from the
+     *   end if the array using built-in .pop() method in your programming 
+     *   language of choise.
+     * - Push elements to the top of the stack by appending elements to the end
+     *   of the array using the built-in .append() method in your programming
+     *   language of your choise.
+     * - Peek at the element on top of the stack by accessing the last element
+     *   in the array.
+     * You're not allowed to perform any other operations on the input array,
+     * including accessing elements(except for the last element) moving elements,
+     * etc.. You're also not allowed to use any other data structures, and your
+     * solution must be recursive.
+     */
+
     // peek returns the top element of stack
     public Integer peek(ArrayList<Integer> stack){
         return stack.get(stack.size() - 1);
