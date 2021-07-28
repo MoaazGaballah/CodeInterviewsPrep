66608aee6887d664f23d8bbf7bd5fb649f407bcc
moaaz GABALLAH <gabalalh.moaaz@gmail.com>
2021-07-28 10:25:33 +0300

    Adding problem description

--- src/AlgoExpret/LargestRectangleUnderSkyline/LargestRectangle.java
+++ src/AlgoExpret/LargestRectangleUnderSkyline/LargestRectangle.java
@@ -11,6 +11,20 @@ public class LargestRectangle {
      * building, including just one building. Note that all building have the
      * same width of 1 unit.
      *
+     * For example, given buildings = [2, 1, 2], the area of the largest
+     * rectangle that can be created is 3, using all three buildings. Since the
+     * minimum height of the three buildings is 1, you can create a rectangle
+     * that has a height of 1 and a width of 3 (the number of buildings). You
+     * could also create rectangles of area 2 by using only the first building
+     * or the last building, but these clearly wouldn't be the largest rectangles.
+     * Similarly, you could create rectangles of area 2 by using the first and
+     * second building or the second and third building.
+     *
+     * To clarify, the width of a created rectangle is the number of building used
+     * to create the rectangle, and its height is the height of the smallest
+     * building used to create it.
+     *
+     * Note that if no rectangles can be created, your function should return 0.
      */
 
     public int largestRectangleUnderSkyline1(ArrayList<Integer> buildings) {
