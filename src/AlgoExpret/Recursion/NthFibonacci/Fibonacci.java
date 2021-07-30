1b6c64e41a176d544003cb130651744cbe5c64da
moaaz GABALLAH <gabalalh.moaaz@gmail.com>
2021-07-30 15:25:36 +0300

    Problem description

--- src/AlgoExpret/Recursion/NthFibonacci/Fibonacci.java
+++ src/AlgoExpret/Recursion/NthFibonacci/Fibonacci.java
@@ -2,6 +2,17 @@ package AlgoExpret.Recursion.NthFibonacci;
 
 public class Fibonacci {
 
+    /*
+     * Problem: The Fibonacci sequence is defined as follows: the first number
+     * of the sequence is 0, the second number is 1, and the nth number is the
+     * sum of the (n-1)th and (n-2)th numbers. Write a function that takes in an
+     * n and returns the nth Fibonacci number.
+     *
+     * Important note: the Fibonacci sequence is often defined with its first two
+     * numbers as F0 = 0 and F1 = 1. For the purpose of this question, the first
+     * Fibonacci number is F0, therefore, getNthFib(1) is equal to F1, etc..
+     */
+
     public static int getNthFib(int n) {
         // Write your code here.
 
